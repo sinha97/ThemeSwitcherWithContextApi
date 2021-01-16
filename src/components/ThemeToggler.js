78c7b654105b88bd8a8c7b657ce96448925efc3a
sinha97 <vk9633698@gmail.com>
2021-01-16 10:23:27 +0530

    ThemeSwitcher

---
+++ src/components/ThemeToggler.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import ThemeContext from '../Context/ThemeContext';
+
+
+const ThemeToggler = () => {
+    const [themeMode, setThemeMode] = useContext(ThemeContext);
+    return (
+        <div onClick={() => {setThemeMode(themeMode === "light" ? "dark" : "light")}}>
+            <span>{themeMode === 'light' ? "Turn Off" : "Lights On"}</span>
+        </div>
+    )
+}
+
+export default ThemeToggler;
